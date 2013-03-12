27b67419da5c2db1a448e8d24931bbd9a2c66ff1
Kazuya NUMATA <numata@gmail.com>
2013-03-12 16:58:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# clojure-hello-world
+
+## Usage
+
+lein run
+
+## License
+
+Copyright © 2013 Kazuya NUMATA
+
+Distributed under MIT License
---
+++ test/clojure_hello_world/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-hello-world.core-test
+  (:use clojure.test
+        clojure-hello-world.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
