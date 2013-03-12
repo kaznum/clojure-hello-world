68beabdadc6b10e82ef7ba625ecf28f6873c91b1
Kazuya NUMATA <numata@gmail.com>
2013-03-12 17:45:26 +0900

    Implement core#hello

--- src/clojure_hello_world/core.clj
+++ src/clojure_hello_world/core.clj
@@ -1,4 +1,10 @@
 (ns clojure-hello-world.core)
 
+(defn hello [x]
+  (str "Hello, " x))
+
 (defn -main []
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (hello "Universe!")))
+
+
